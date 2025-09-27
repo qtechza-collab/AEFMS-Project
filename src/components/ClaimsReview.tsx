c08b85e407989b48f6e11c545fc83c8e2c19567f
MartyB <qtechza@gmail.com>
2025-09-28 01:53:27 +0200

    Update ClaimsReview.tsx

--- src/components/ClaimsReview.tsx
+++ src/components/ClaimsReview.tsx
@@ -34,7 +34,7 @@ import {
   Archive,
   ArrowLeft
 } from 'lucide-react';
-import { toast } from 'sonner@2.0.3';
+import { toast } from 'sonner';
 import { dataStore } from '../utils/dataStore';
 import { ImageViewer } from './ImageViewer';
 import { ClaimApprovalTester } from './ClaimApprovalTester';
