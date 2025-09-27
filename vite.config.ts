12428756ca92df1e735bc6b7eed1bad92277ad67
MartyB <qtechza@gmail.com>
2025-09-27 14:41:52 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: '.',
+  publicDir: 'public',
   plugins: [
     react({
       // Explicit configuration for better compatibility
