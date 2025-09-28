2abf255698fe0cbbefb7b465350c39f158767687
MartyB <qtechza@gmail.com>
2025-09-28 14:48:30 +0200

    Update enhancedClaimsManager.ts

--- src/utils/enhancedClaimsManager.ts
+++ src/utils/enhancedClaimsManager.ts
@@ -1,6 +1,6 @@
 import { dataService } from './supabaseDataService';
 import type { ExpenseClaim } from './supabaseDataService';
-import { toast } from 'sonner@2.0.3';
+import { toast } from 'sonner';
 
 /**
  * Enhanced Claims Manager for Logan Freights
