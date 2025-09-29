5f4c8240b48afb256bf57da8e74ef2bc911bed7d
MartyB <qtechza@gmail.com>
2025-09-30 00:03:08 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './styles/globals.css'
 import './index.css'  // Change from './styles/global.css' 
 
 // Enhanced error handling for production
